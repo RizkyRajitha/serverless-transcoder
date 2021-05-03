bf604afeabf3660eb7f5736127d441f246177a80
rizkyrajitha <rajithagunathilake@gmail.com>
2021-05-03 12:25:12 +0530

    minor error reproduce

--- handler.js
+++ handler.js
@@ -41,11 +41,14 @@ module.exports.hello = async (event, context) => {
     };
   }
 
-  let sourceFileName = decodeURIComponent(
-    event.Records[0].s3.object.key.replace(/\+/g, " ")
-  );
+  console.log(event.Records[0].s3.object.key)
 
-  console.log(sourceFileName);
+  let sourceFileName = event.Records[0].s3.object.key 
+  // decodeURIComponent(
+  //   event.Records[0].s3.object.key.replace(/\+/g, " ")
+  // );
+
+  console.log("source filename - "+sourceFileName);
 
   if (!sourceFileName.endsWith(".mp4")) {
     console.log("file format error");
